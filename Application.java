bf619037a52b40b8800cc88e50618b1c3b7098ea
Pablo Alves <palves@informasoftware.com.br>
2022-06-24 22:06:23 -0300

    Ajustes finais

--- Application.java
+++ Application.java
@@ -1,9 +1,8 @@
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Period;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Application {
 
@@ -12,11 +11,51 @@ public class Application {
         //apiNova();
         //formasInstanciar();
         //apiNovaOperacoes();
-        apiNovaCalculos();
-
+        //apiNovaCalculos();
+        //fusoHorario();
+        exemploInstant();
+        //ex1CalculaIdade();
     }
 
-    public Application() {
+    private static void exemploInstant() {
+        Instant ininial = Instant.now();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Instant instFinal = Instant.now();
+        Duration duration = Duration.between(ininial, instFinal);
+        System.out.println("Tempo execução: " + duration.toSeconds());
+    }
+
+    private static void fusoHorario() {
+        var dataHoraAtual = LocalDateTime.of(2022, Month.JUNE, 20, 22, 00);
+        ZoneId fusoSaoPaulo = ZoneId.of("America/Sao_Paulo");
+        ZonedDateTime horaSaoPaulo = ZonedDateTime.of(dataHoraAtual, fusoSaoPaulo);
+        System.out.println("Hora São Paulo: " + horaSaoPaulo);
+
+
+        ZoneId fusoParis = ZoneId.of("Europe/Paris");
+        LocalDateTime hora2 = LocalDateTime.now(fusoParis);
+        System.out.println("Teste hora paris: " + hora2);
+
+        ZonedDateTime horaParis = ZonedDateTime.of(dataHoraAtual, fusoParis);
+        System.out.println("Hora Paris: " + horaParis);
+
+        Duration diferencaHoras = Duration.between(hora2, LocalDateTime.now());
+        System.out.println("Diferença de fuso SP/Paris: " + diferencaHoras.toHours());
+    }
+
+    private static void ex1CalculaIdade() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Digite a data de seu nascimento (dd/mm/yyyy): ");
+
+        var dataAtual = LocalDate.now();
+
+        LocalDate nascimento = LocalDate.parse(scanner.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        Period period = Period.between(nascimento, dataAtual);
+        System.out.printf("Idade: %d anos %d meses %d dias", period.getYears(), period.getMonths(), period.getDays());
     }
 
     private static void apiNovaCalculos() {
@@ -61,6 +100,7 @@ public class Application {
     private static void apiAntiga() {
         Date date = new Date(2022-1900, 11, 10, 16, 40);
         System.out.println(date.toString());
+        System.out.println("Tempo: " + date.getTime());
     }
 
 }
